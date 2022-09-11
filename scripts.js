906548aaf3ae7e09a0e4e92dd232cf3cda366198
desh-89 <97294748+desh-89@users.noreply.github.com>
2022-09-11 14:12:31 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+      content.style.padding = 0 + "px";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.padding = 18 + "px";
+    }
+  });
+}
